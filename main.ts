3ac07df768dd47659b3dce8cbbf73c8cfeaf0bde
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-04 13:21:32 -0500

    gf

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan
+ * Created on: Mar 2026
+ * This program will make cookies
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+let cookies: number = 0
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//functions
+input.onButtonPressed(Button.A, function () {
+    cookies = cookies + 1
+    basic.showNumber(cookies)
+})
+
+input.onButtonPressed(Button.B, function () {
+    cookies = 0
+    basic.showNumber(cookies)
+})
+
